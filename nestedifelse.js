4c10c303810ab7f9ff8c13b5abbc3511be159ee7
rohit1851995 <rohit1851995@gmail.com>
2025-07-20 19:55:10 +0000

    nested

---
+++ nestedifelse.js
@@ -0,0 +1,16 @@
+let userSubscribed = true;
+let userHasLionsGatePlay = false;
+let userHasSony = false;
+if (userSubscribed){
+    if(userHasLionsGatePlay){
+        console.log("watch john wick");
+        
+    } else if(userHasSony){
+        console.log("show the gurdian");
+        
+    } else {
+        console.log("show panchayat");
+        
+    }
+
+}
